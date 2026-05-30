OtterBrowser/otter-browser

qt6: rename downloadrequest members

src/modules/backends/web/qtwebengine/QtWebEngineTransfer.cpp
@@ -34,8 +34,10 @@ QtWebEngineTransfer::QtWebEngineTransfer(QWebEngineDownloadRequest *item, Transf
 
 	markAsStarted();
 
-	connect(m_item, &QWebEngineDownloadRequest::finished, this, &QtWebEngineTransfer::markAsFinished);
-	connect(m_item, &QWebEngineDownloadRequest::downloadProgress, this, &QtWebEngineTransfer::handleDownloadProgress);
+	connect(m_item, &QWebEngineDownloadRequest::isFinished, this, &QtWebEngineTransfer::markAsFinished);
+	/* qt6: cannot take the address of an rvalue of type 'QWebEngineDownloadRequest::DownloadState'
+	connect(m_item, &QWebEngineDownloadRequest::DownloadInProgress, this, &QtWebEngineTransfer::handleDownloadProgress);
+	*/
 	connect(m_item, &QWebEngineDownloadRequest::stateChanged, this, [&](QWebEngineDownloadRequest::DownloadState state)
 	{
 		switch (state)

jjtmp/otter1

src/modules/backends/web/qtwebengine/QtWebEngineTransfer.cpp
@@ -40,8 +40,10 @@ QtWebEngineTransfer::QtWebEngineTransfer(QWebEngineDownloadX *item, TransferOpti
 
 	markAsStarted();
 
-	connect(m_item, &QWebEngineDownloadX::finished, this, &QtWebEngineTransfer::markAsFinished);
-	connect(m_item, &QWebEngineDownloadX::downloadProgress, this, &QtWebEngineTransfer::handleDownloadProgress);
+	connect(m_item, &QWebEngineDownloadX::isFinished, this, &QtWebEngineTransfer::markAsFinished);
+	/* qt6: cannot take the address of an rvalue of type 'QWebEngineDownloadRequest::DownloadState'
+	connect(m_item, &QWebEngineDownloadX::DownloadInProgress, this, &QtWebEngineTransfer::handleDownloadProgress);
+	*/
 	connect(m_item, &QWebEngineDownloadX::stateChanged, this, [&](QWebEngineDownloadX::DownloadState state)
 	{
 		switch (state)